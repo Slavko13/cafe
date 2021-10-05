b38fe8fec169829f7bc2d7247340fa106b7adb29
firep <341234>
2021-10-05 14:17:34 +0300

    Сделал полностью локализацию нормальным способом, пофиксил стили

--- src/main/java/com/cafe/cafe/controller/LocaleBean.java
+++ src/main/java/com/cafe/cafe/controller/LocaleBean.java
@@ -5,17 +5,43 @@ import lombok.Data;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 
 @Scope(value = "session")
 @ManagedBean
 @Data
 public class LocaleBean {
 
-        private String currentLocale = "eng";
+        private String locale = "en";
+        private static Locale RUSSIA = new Locale("ru");
+
+        private static Map<String,Object> countries;
+        static {
+
+                countries = new LinkedHashMap<String,Object>();
+                countries.put("Russian", RUSSIA);
+                countries.put("English", Locale.ENGLISH);
+        }
+
+        public Map<String, Object> getCountries() {
+                return countries;
+        }
+
+        public void updateCurrentLocale(String language) {
+                locale=language;
+                for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+                        if(entry.getValue().toString().equals(language)) {
+                                FacesContext.getCurrentInstance()
+                                        .getViewRoot().setLocale((Locale) entry.getValue());
+                        }
+                }
 
 
-        public void updateCurrentLocale(String localeToChange) {
-                currentLocale = localeToChange;
         }
 
 }
