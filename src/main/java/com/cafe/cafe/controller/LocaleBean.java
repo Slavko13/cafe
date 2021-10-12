f75dcb540b60c204ee9a4ca67692b108d2e0656b
firep <341234>
2021-10-12 10:40:03 +0300

    Стили на кнопки все сделал

--- src/main/java/com/cafe/cafe/controller/LocaleBean.java
+++ src/main/java/com/cafe/cafe/controller/LocaleBean.java
@@ -17,21 +17,29 @@ public class LocaleBean {
 
         private String locale = "en";
         private static Locale RUSSIA = new Locale("ru");
+        private String currentLanguage = "English";
 
 
         private static Map<String,Object> countries;
 
 
-
         static {
-
                 countries = new LinkedHashMap<String,Object>();
-                countries.put("Russian", RUSSIA);
+                countries.put("Русский", RUSSIA);
                 countries.put("English", Locale.ENGLISH);
         }
 
         public Map<String, Object> getCountries() {
                 return countries;
+        }
+
+        public String getCurrentLocale() {
+                for (Map.Entry<String, Object> entry : countries.entrySet()) {
+                        if(entry.getValue().toString().equals(locale)) {
+                                return  entry.getKey();
+                        }
+                }
+                return null;
         }
 
         public void updateCurrentLocale(String language) {
